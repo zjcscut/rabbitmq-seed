3b63cb3f17eb330de7e461ee17f79c89893ff4ad
zjc <15118850470>
2018-01-22 23:33:23 +0800

    Initial commit

---
+++ src/main/java/org/throwable/rabbitmq/ch1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.throwable.rabbitmq.ch1;
+
+/**
+ * @author throwable
+ * @version v1.0
+ * @description
+ * @since 2018/1/22 23:29
+ */
+public class HelloWorld {
+
+	public static void main(String[] args){
+
+	}
+}
---
+++ src/main/java/org/throwable/rabbitmq/ch1/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @author throwable
+ * @version v1.0
+ * @description hello world
+ * @since 2018/1/22 23:28
+ */
+package org.throwable.rabbitmq.ch1;
